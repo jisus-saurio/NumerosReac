f146154973d38bc7d811d0a197a0157ba21748dc
stanlyy <stanleyarce51@gmail.com>
2025-03-21 08:05:49 -0600

    Numeros

---
+++ diseño/src/components/Numeros.jsx
@@ -0,0 +1,13 @@
+import "./numeros.css";
+
+const Numeros = () => {
+    return (
+        <div class="container">
+     <h1>Números</h1>
+     <input type="text" placeholder="Número 1"/>
+     <br />
+    <input type="text" placeholder="Número 2"/>
+        </div>
+    );
+    }
+    export default Numeros;
